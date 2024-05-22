80851f595931234de6bfc092bb9b28e43eeadd24
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-05-22 15:40:42 +0200

    - Delete unused and heavy graphics - Add v1 lighting - Firelight shaker script - Skybox

---
+++ Assets/Scripts/Graphics/FireLightShaker.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FireLightShaker : MonoBehaviour
+{
+    private Light light;
+    private float timer = 0.0f;
+    private float maxTimer = 3.0f;
+    private float minIntensity;
+    public float maxIntensity;
+    private float minRange;
+    public float maxRange;
+    private AnimationCurve curve = new AnimationCurve(
+            new Keyframe(0.0f, 0.0f),
+            new Keyframe(1.5f, 1.0f),
+            new Keyframe(3.0f, 0.0f));
+
+    private void Awake()
+    {
+        light = GetLight();
+        timer = Random.Range(0.0f, maxTimer);
+        minIntensity = light.intensity;
+        minRange = light.range;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > maxTimer)
+            timer = 0.0f;
+        
+        light.intensity = Mathf.Lerp(minIntensity, maxIntensity, curve.Evaluate(timer));
+        light.range = Mathf.Lerp(minRange, maxRange, curve.Evaluate(timer));
+    }
+
+    private Light GetLight()
+    {
+        return GetComponent<Light>();
+    }
+}
