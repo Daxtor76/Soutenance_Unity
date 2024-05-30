27fe6510acfe71597e35f75d3dec02cb40a3d06f
QuentinTilak <lecavorzin.quentin@gmail.com>
2024-05-30 15:56:36 +0200

    v1 strafing camera effect

--- Assets/Scripts/Components/CameraController.cs
+++ Assets/Scripts/Components/CameraController.cs
@@ -13,6 +13,7 @@ public class CameraController : MonoBehaviour
 
     private Vector3 _currentVelocity;
 
+    // FOV acceleration effect
     private bool _canInterpolateFOV = false;
     private float _kyubiFOVTransitionSpeed = 3f;
     private int _kyubiFOVTransitionSign = -1;
@@ -20,11 +21,18 @@ public class CameraController : MonoBehaviour
     private float _cameraMaxFOV = 70.0f;
     private float _FOVInterpolator = 0.0f;
 
+    // Rotation Strafe effect
+    float rotator = 0.0f;
+    private bool _canRotate = false;
+    private float _minRotation = -5.0f;
+    private float _maxRotation = 5.0f;
+    private float _rotationSpeed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         _target = GameObject.Find(Const.CHARACTER_NAME).transform.Find(Const.CHARACTER_CAMERA_DUMMY);
-        _character = transform.parent.gameObject.GetComponent<Actor>();
+        _character = transform.parent.transform.parent.gameObject.GetComponent<Actor>();
         _camera = GetComponent<Camera>();
         _posFromTarget = transform.position - _target.position;
         _character.StateController.OnStateChange.AddListener(AdaptFromStateChange);
@@ -45,10 +53,13 @@ public class CameraController : MonoBehaviour
             if (_camera.fieldOfView == _cameraMinFOV || _camera.fieldOfView == _cameraMaxFOV)
                 _canInterpolateFOV = false;
         }
-    }
 
-    private void LateUpdate()
-    {
+        // Rotate when strafe
+        rotator += _rotationSpeed * Time.deltaTime * -Input.GetAxisRaw(Const.STRAFE_AXIS_NAME);
+        rotator = Mathf.Clamp(rotator, _minRotation, _maxRotation);
+        if (Input.GetAxisRaw(Const.STRAFE_AXIS_NAME) == 0.0f)
+            rotator = 0.0f;
+        transform.parent.rotation *= Quaternion.Euler(0.0f, 0.0f, rotator);
     }
 
     private void AdaptFromStateChange(Actor.States newState)
